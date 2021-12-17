857cfb44cfcb1c9109a25b4eeed92bc435a8f73c
cplee1 <c.p.lee@student.curtin.edu.au>
2021-12-17 12:08:51 +0800

    Made a new model double_broken_power_law

--- pulsar_spectra/models.py
+++ pulsar_spectra/models.py
@@ -16,6 +16,16 @@ def broken_power_law(v, a1, a2, b, vb):
     y2 = b*x**a2*(xb)**(a1-a2)
     return np.where(x <= xb, y1, y2)
 
+def double_broken_power_law(v, a1, a2, a3, b, vb1, vb2):
+    v0 = 1.3e9
+    x = v / v0
+    xb1 = vb1 / v0
+    xb2 = vb2 / v0
+    y1 = b*x**a1
+    y2 = b*x**a2*(xb1)**(a1-a2)
+    y3 = b*x**a3*(xb2)**(a2-a3)
+    return np.piecewise(x, [x <= xb1, (x > xb1) & (x <= xb2), x > xb2], [y1, y2, y3])
+
 def log_parabolic_spectrum(v, a, b, c):
     v0 = 1.3e9
     x = np.log10( v / v0 )
