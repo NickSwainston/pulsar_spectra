0f958ccc85c792f010301d077a158d64c0cba55c
NickSwainston <nickaswainston@gmail.com>
2022-01-03 20:00:32 +0800

    Make the test Figure 5 of Jankowski et al. (2018)

--- tests/test_spectral_fit.py
+++ tests/test_spectral_fit.py
@@ -21,7 +21,8 @@ def test_find_best_spectral_fit():
     """
     cat_dict, cat_list = collect_catalogue_fluxes()
     #print(cat_dict)
-    pulsars = ['J0034-0534','J0953+0755', 'J1645-0317']
+    #pulsars = ['J0034-0534','J0953+0755', 'J1645-0317']
+    pulsars = ['J0837+0610', 'J1359-6038', 'J0908-4913', 'J1903-0632']
     for pulsar in pulsars:
         print(f"\nFitting {pulsar}")
         #print(cat_dict[pulsar])
