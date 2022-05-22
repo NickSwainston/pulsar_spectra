d2ceb44838f8bc50f09ced8b56b51aef08cb2354
Nick Swainston <nickaswainston@gmail.com>
2022-05-22 13:12:00 +0800

    Update version to 1.3

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ download_ANTF_pulsar_database_file(datadir)
 
 
 #pulsar_spectra_version = get_git_version()
-pulsar_spectra_version = '1.2.0'
+pulsar_spectra_version = '1.3.0'
 #make a temporary version file to be installed then delete it
 with open('version.py', 'a') as the_file:
     the_file.write('__version__ = "{}"\n'.format(pulsar_spectra_version))
