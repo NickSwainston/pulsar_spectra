f253054caa5579a277078c6c6346b4fa79de6dd4
Nick <nickaswainston@gmail.com>
2022-06-15 09:35:30 +0800

    Updated all the models so they use c as the scaling constant and thier doc string equations are the same as the paper

--- pulsar_spectra/models.py
+++ pulsar_spectra/models.py
@@ -4,13 +4,11 @@ Spectral models from Jankowski et al. 2018 and references within
 
 import numpy as np
 
-def simple_power_law(v, a, b, v0):
+def simple_power_law(v, a, c, v0):
     """Simple power law:
 
     .. math::
-        S_v = b x^a
-
-    where :math:`x=\\frac{v}{1.3e9}`
+        S_v =  c \left( \frac{v}{v_0} \right)^a
 
     Parameters
     ----------
@@ -18,7 +16,7 @@ def simple_power_law(v, a, b, v0):
         Frequency in Hz.
     a : `float`
         Spectral Index.
-    b : `float`
+    c : `float`
         Constant.
     v0 : `float`
         Reference frequency.
@@ -28,27 +26,28 @@ def simple_power_law(v, a, b, v0):
     S_v : `list`
         The flux density predicted by the model.
     """
-    return b*(v/v0)**a
+    return c*(v/v0)**a
 
-def broken_power_law(v, vb, a1, a2, b, v0):
+def broken_power_law(v, vb, a1, a2, c, v0):
     """Broken power law:
 
     .. math::
-        S_v = b \left\{\\begin{matrix} x^{a_1} & \mathrm{if} x ≤ x_b \\\\ x^{a_2}x_b^{a_1-a_2} & \mathrm{otherwise'} \end{matrix}\\right.
-
-    where :math:`x=\\frac{v}{1.3e9},x_b=\\frac{v_b}{1.3e9}`
+        S_v = \begin{cases}
+        c \left( \frac{v}{v0} \right)^{a1}   & \mathrm{if} v \leq vb \\[5pt]
+        c \left( \frac{v}{v0} \right)^{a2} \left( \frac{vb}{v0} \right)^{a1-a2} & \mathrm{otherwise} \\
+        \end{cases}
 
     Parameters
     ----------
     v : `list`
         Frequency in Hz.
-    v_b : `float`
+    vb : `float`
         The frequency of the break in Hz.
-    a_1 : `float`
+    a1 : `float`
         The spectral index before the break.
-    a_2 : `float`
+    a2 : `float`
         The spectral index after the break.
-    b : `float`
+    c : `float`
         Constant.
     v0 : `float`
         Reference frequency.
@@ -60,26 +59,25 @@ def broken_power_law(v, vb, a1, a2, b, v0):
     """
     x = v / v0
     xb = vb / v0
-    y1 = b*x**a1
-    y2 = b*x**a2*(xb)**(a1-a2)
+    y1 = c*x**a1
+    y2 = c*x**a2*(xb)**(a1-a2)
     return np.where(x <= xb, y1, y2)
 
-def double_broken_power_law(v, vb1, vb2, a1, a2, a3, b, v0):
+def double_broken_power_law(v, vb1, vb2, a1, a2, a3, c, v0):
     x = v / v0
     xb1 = vb1 / v0
     xb2 = vb2 / v0
     return np.piecewise(x, [x <= xb1, (x > xb1) & (x <= xb2), x > xb2], \
-    [lambda x: b*x**a1, \
-     lambda x: b*x**a2*(xb1)**(a1-a2), \
-     lambda x: b*x**a3*(xb1)**(a1-a2)*(xb2)**(a2-a3)])
+    [lambda x: c*x**a1, \
+     lambda x: c*x**a2*(xb1)**(a1-a2), \
+     lambda x: c*x**a3*(xb1)**(a1-a2)*(xb2)**(a2-a3)])
 
 def log_parabolic_spectrum(v, a, b, c, v0):
     """Log-parabolic spectrum:
 
     .. math::
-       \mathrm{log}_{10} S_v = ax^2 + bx +c
-
-    where :math:`x=\mathrm{log}_{10} \left ( \\frac{v}{1.3e9} \\right )`
+        \mathrm{log}_{10} S_v = a  \left [ \mathrm{log}_{10} \left ( \frac{v}{v0} \right ) \right]^2 +
+        b \, \mathrm{log}_{10} \left ( \frac{v}{v0} \right ) + c
 
     Parameters
     ----------
@@ -102,21 +100,19 @@ def log_parabolic_spectrum(v, a, b, c, v0):
     x = np.log10( v / v0 )
     return 10**(a*x**2 + b*x + c)
 
-def high_frequency_cut_off_power_law(v, vc, b, v0):
+def high_frequency_cut_off_power_law(v, vc, c, v0):
     """Power law with high-frequency cut-off off:
 
     .. math::
-        S_v = bx^{-2} \left ( 1 - \\frac{x}{x_c} \\right ), x < x_c
-
-    where :math:`x=\\frac{v}{1.3e9},x_c=\\frac{v_c}{1.3e9}`
+        S_v = c \left( \frac{v}{v0} \right)^{-2} \left ( 1 - \frac{v}{vc} \right ),\qquad v < v_c
 
     Parameters
     ----------
     v : `list`
         Frequency in Hz.
-    v_c : `list`
+    vc : `list`
         Cut off frequency in Hz.
-    b : `float`
+    c : `float`
         Constant.
     v0 : `float`
         Reference frequency.
@@ -128,27 +124,25 @@ def high_frequency_cut_off_power_law(v, vc, b, v0):
     """
     x = v / v0
     xc = vc / v0
-    y1 = b*x**(-2) * ( 1 - x / xc )
+    y1 = c*x**(-2) * ( 1 - x / xc )
     y2 = 0
     return np.where(x < xc, y1, y2)
 
-def low_frequency_turn_over_power_law(v, vc, a, b, beta, v0):
+def low_frequency_turn_over_power_law(v, vc, a, c, beta, v0):
     """power law with low-frequency turn-over:
 
     .. math::
-        S_v = bx^{a} exp\left ( \\frac{a}{\\beta} x_c^{-\\beta} \\right )
-
-    where :math:`x=\\frac{v}{1.3e9},x_c=\\frac{v_c}{1.3e9}`
+        S_v = c \left( \frac{v}{v0} \right)^{a} \exp\left [ \frac{a}{beta} \left( \frac{v}{vc} \right)^{-beta} \right ]
 
     Parameters
     ----------
     v : `list`
         Frequency in Hz.
-    v_c : `list`
+    vc : `list`
         Trun-over frequency in Hz.
     a : `float`
         The spectral index.
-    b : `float`
+    c : `float`
         Constant.
     beta : `float`
         The smoothness of the turn-over.
@@ -162,7 +156,7 @@ def low_frequency_turn_over_power_law(v, vc, a, b, beta, v0):
     """
     x = v / v0
     xc = v / vc
-    return b * x**a * np.exp( a / beta * xc**(-beta) )
+    return c * x**a * np.exp( a / beta * xc**(-beta) )
 
 
 def model_settings(print_models=False):
