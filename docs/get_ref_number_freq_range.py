c795cc72cf2147d7283b3eaacfc894031ac42799
Nick <nickaswainston@gmail.com>
2022-07-30 21:31:40 +0800

    Added Qiao et al. (1995)

--- docs/get_ref_number_freq_range.py
+++ docs/get_ref_number_freq_range.py
@@ -90,6 +90,7 @@ ads_dict = {
     "Robinson_1995": "https://ui.adsabs.harvard.edu/abs/1995MNRAS.274..547R/abstract",
     "McConnell_1991": "https://ui.adsabs.harvard.edu/abs/1991MNRAS.249..654M/abstract",
     "Manchester_1996": "https://ui.adsabs.harvard.edu/abs/1996MNRAS.279.1235M/abstract",
+    "Qiao_1995": "https://ui.adsabs.harvard.edu/abs/1995MNRAS.274..572Q/abstract",
 }
 # Loop over catalogues and put them into a dictionary
 for cat_file in CAT_YAMLS:
