e88f08c36f849b28a1e46a5764f61db7bee8d6da
Sammy McSweeney <sammy.mcsweeney@gmail.com>
2023-06-13 20:01:42 +0800

    Corrected typo in model equation in docs

--- pulsar_spectra/models.py
+++ pulsar_spectra/models.py
@@ -409,7 +409,7 @@ def double_turn_over_spectrum(v, vc, vpeak, a, beta, c, v0):
     """Double turn over spectrum (has a low-frequency turnover and a high-frequency cut-off):
 
     .. math::
-        S_v = c \\left( \\frac{v}{v0} \\right)^{a} \\left ( 1 - \\frac{v}{vc} \\right ) \\exp\\left [ \\frac{a}{\\beta} \\left( \\frac{v}{vc} \\right)^{-\\beta} \\right ],\\qquad v < vc
+        S_v = c \\left( \\frac{v}{v0} \\right)^{a} \\left ( 1 - \\frac{v}{vc} \\right ) \\exp\\left [ \\frac{a}{\\beta} \\left( \\frac{v}{vpeak} \\right)^{-\\beta} \\right ],\\qquad v < vc
 
     Parameters
     ----------
