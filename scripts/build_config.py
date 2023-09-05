16e0bd3bce490c3e22da2b9499f6c9abca6d3da1
cplee1 <c.p.lee@student.curtin.edu.au>
2023-09-05 14:38:16 +0800

    Added option to generate markers

--- scripts/build_config.py
+++ scripts/build_config.py
@@ -3,13 +3,67 @@
 # TODO: setup logging
 
 import os
-from optparse import OptionParser, OptionGroup
+import csv
+import random
 import yaml
+from optparse import OptionParser, OptionGroup
 import numpy as np
 import matplotlib.colors as mcolors
 import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
 
 from pulsar_spectra.load_data import DEFAULT_PLOTTING_CONFIG, DEFAULT_MARKER_CSV
+
+
+# Colour Pallettes from https://davidmathlogic.com/colorblind/
+PALLETTE_IBM = [
+    ['#648FFF', 'blue'],
+    ['#785EF0', 'dark lavender'],
+    ['#DC267F', 'deep pink'],
+    ['#FE6100', 'dark orange'],
+    ['#FFB000', 'orange'],
+    ['#FDDF49', 'yellow'],
+    ['#8FB327', 'lime green'],
+    ['#902499', 'purple'],
+    ['#17E474', 'lime'],
+    ['#74CE22', 'green'],
+    ['#449C8C', 'teal'],
+    ['#EA83B7', 'pink'],
+]
+
+PALLETTE_WONG = [
+    ['#DF0A10', 'red'],
+    ['#E69F00', 'orange'],
+    ['#56B4E9', 'sky blue'],
+    ['#009E73', 'sea green'],
+    ['#F0E442', 'yellow'],
+    ['#0072B2', 'blue'],
+    ['#D55E00', 'dark orange'],
+    ['#CC79A7', 'pink'],
+    ['#80C774', 'lime green'],
+    ['#A21DCA', 'purple'],
+    ['#9BFB5F', 'lawn green'],
+    ['#BDD1D2', 'grey'],
+]
+
+# Marker size fudge factors are for perceptual uniformity
+MARKERS = [
+    ['o', 0.95, 'circle'],
+    ['v', 1, 'down-pointing triangle'],
+    ['^', 1, 'up-pointing triangle'],
+    ['<', 1, 'left-pointing triangle'],
+    ['>', 1, 'right-pointing triangle'],
+    ['8', 1, 'octagon'],
+    ['s', 0.9, 'square'],
+    ['p', 1, 'pentagon'],
+    ['P', 1.2, 'plus'],
+    ['*', 1.5, 'star'],
+    ['h', 1, 'hexagon'],
+    ['H', 1, 'rotated hexagon'],
+    ['X', 1, 'cross'],
+    ['D', 0.75, 'diamond'],
+    ['d', 0.9, 'thin diamond'],
+]
 
 
 def parse_opts():
@@ -18,7 +72,7 @@ def parse_opts():
     
     output_options = OptionGroup(parser, 'Output Options')
     output_options.add_option('-F', '--filename',
-                  action='store', type='string', dest='output_file', default=DEFAULT_PLOTTING_CONFIG,
+                  action='store', type='string', dest='output_file', default='../pulsar_spectra/configs/plotting_config.yaml',
                   help='Location of output configuration file to write to [default: %default]')
     parser.add_option_group(output_options)
 
@@ -59,9 +113,9 @@ def parse_opts():
                   action='store', type='string', dest='marker_file',
                   default=DEFAULT_MARKER_CSV,
                   help='List of marker styles [default: %default]')
-    marker_config.add_option('--marker_scale',
-                  action='store', type='float', dest='marker_scale', default=0.7,
-                  help='Scaling factor for marker size [default: %default]')
+    marker_config.add_option('--marker_size',
+                  action='store', type='float', dest='marker_size', default=4.5,
+                  help='Marker size [default: %default]')
     marker_config.add_option('--marker_border_lw',
                   action='store', type='float', dest='marker_border_lw', default=0.5,
                   help='Marker border thickness [default: %default]')
@@ -72,6 +126,31 @@ def parse_opts():
                   action='store', type='float', dest='errorbar_lw', default=0.7,
                   help='Errorbar line width [default: %default]')
     parser.add_option_group(marker_config)
+
+    marker_generation = OptionGroup(parser, 'Marker Generation Options')
+    marker_config.add_option('-g', '--generate_markers',
+                  action='store_true', dest='generate', default=False,
+                  help='Generate a set of unique markers.')
+    marker_config.add_option('--shuffle',
+                  action='store_true', dest='shuffle', default=False,
+                  help='Randomise the marker colour/type order.')
+    marker_config.add_option('--no_scaling',
+                  action='store_false', dest='uniform_size', default=True,
+                  help='Do not use perceptually uniform marker sizes.')
+    marker_config.add_option('--marker_preview',
+                  action='store_true', dest='plot_preview', default=False,
+                  help='Plot a preview of the generated marker types.')
+    marker_generation.add_option('--num_markers',
+                  action='store', type='int', dest='num_markers', default=30,
+                  help='Number of unique markers to generate [default: %default]')
+    marker_config.add_option('--pallette',
+                  action='store', type='string', dest='pallette',
+                  default='IBM',
+                  help="Colour pallette to use. Available pallettes: 'IBM', 'WONG' [default: %default]")
+    marker_config.add_option('--marker_file_savename',
+                  action='store', type='string', dest='marker_file_savename',
+                  default=None,
+                  help='Write generated markers to the specified file [default: %default]')
     
     (opts, _) = parser.parse_args()
 
@@ -107,6 +186,79 @@ def is_valid_marker(marker_string):
     return marker_string in valid_markers
 
 
+def generate_marker_set(num_markers, marker_size, pallette_name='IBM',
+                        shuffle=True, uniform_size=True, plot_preview=False,
+                        savename=None):
+    if pallette_name == 'IBM':
+        pallette = PALLETTE_IBM
+    elif pallette_name == 'WONG':
+        pallette = PALLETTE_WONG
+    else:
+        print(f'Error: pallette name not valid. Defaulting to IBM.')
+        pallette = PALLETTE_IBM
+
+    if shuffle:
+        random.shuffle(pallette)
+        random.shuffle(MARKERS)
+
+    if num_markers < len(pallette):
+        colours = pallette[0:num_markers]
+    else:
+        colours = (num_markers//len(pallette))*pallette + pallette[0:len(pallette)%num_markers]
+
+    if num_markers < len(MARKERS):
+        markers = MARKERS[0:num_markers]
+    else:
+        markers = (num_markers//len(MARKERS))*MARKERS + MARKERS[0:len(MARKERS)%num_markers]
+
+    if plot_preview:
+        # Preview markers as a sinusoid
+        fig, ax = plt.subplots(figsize=(4,2))
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_ylim([-1.5, 1.5])
+        x_arr = np.arange(num_markers)*2*np.pi/num_markers
+        y_arr = np.sin(x_arr)
+    
+    unique_markers = []
+    for i in range(num_markers):
+        marker = [f'{colours[i][1]} {markers[i][2]}', colours[i][0], markers[i][0], round(marker_size*markers[i][1], 2)]
+        if marker in unique_markers:
+            print(f'Duplicate marker skipped: {marker}')
+            continue
+        unique_markers.append(marker)
+        
+        if plot_preview:
+            ax.errorbar(
+                x_arr[i],
+                y_arr[i],
+                yerr=0.1,
+                xerr=0.1,
+                c=markers[1],
+                marker=markers[2],
+                ms=marker_size*markers[3],
+                ls=None,
+                mec='k',
+                mew=0.5,
+                elinewidth=0.7,
+                capsize=1.5,
+            )
+
+    if savename:
+        with open(savename, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['%marker_colour', 'marker_type', 'marker_size', 'description'])
+            for marker in unique_markers:
+                writer.writerow([marker[1], marker[2], marker[3], marker[0]])
+    
+    if plot_preview:
+        plt.savefig(f'marker_preview.png', dpi=300, bbox_inches='tight')
+        plt.clf()
+
+    return unique_markers
+
+
+
 def main():
     opts = parse_opts()
 
@@ -122,17 +274,25 @@ def main():
     config["Marker border"] = opts.marker_border_lw
     config["Capsize"] = opts.capsize
     config["Errorbar linewidth"] = opts.errorbar_lw
-    config["Markers"] = []
-    marker_styles = np.loadtxt(opts.marker_file, delimiter=',', dtype=str, comments='%')
-    for style in marker_styles:
-        if not is_valid_colour(style[0]) and not is_valid_marker(style[1]):
-            print(f'Error: invalid marker style - {style} (skipping)')
-            continue
-        if style[3] == '':
-            desc = 'Unlabelled'
-        else:
-            desc = str(style[3])
-        config["Markers"].append([desc, str(style[0]), str(style[1]), round(float(style[2])*opts.marker_scale,2)])
+
+    if opts.generate:
+        markers = generate_marker_set(opts.num_markers, opts.marker_size,
+            opts.pallette, opts.shuffle, opts.uniform_size, opts.plot_preview,
+            opts.marker_file_savename)
+    else:
+        markers = []
+        marker_styles = np.loadtxt(opts.marker_file, delimiter=',', dtype=str, comments='%')
+        for style in marker_styles:
+            if not is_valid_colour(style[0]) and not is_valid_marker(style[1]):
+                print(f'Error: invalid marker style - {style} (skipping)')
+                continue
+            if style[3] == '':
+                desc = 'Unlabelled'
+            else:
+                desc = str(style[3])
+            markers.append([desc, str(style[0]), str(style[1]), round(float(style[2])*opts.marker_scale,2)])
+
+    config["Markers"] = markers
 
     with open(opts.output_file, 'w') as f:
         yaml.dump(config, f, sort_keys=False)
