8c65f7b4875371b3cccbce07fe741b90448df310
cplee1 <c.p.lee@student.curtin.edu.au>
2025-06-10 14:31:10 +0800

    Updated requirements to Python>=3.8 and incremented minor version

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@
 """
 Setup for pulsar_spectra
 """
+
 import re
 from setuptools import setup
 
 reqs = [
-    'numpy>=1.13.3',
-    'matplotlib>=3.4.0',
-    'psrqpy>=1.2.4',
-    'iminuit>=2.11.1',
-    'jacobi',
-    'PyYAML',
-    'panda<=1.3.5',
+    "numpy>=1.13.3",
+    "matplotlib>=3.7.5",
+    "psrqpy>=1.3.2",
+    "iminuit>=2.27.0",
+    "jacobi>=0.9.2",
+    "PyYAML>=6.0.2",
+    "panda<=1.3.5",
 ]
 
-VERSIONFILE="pulsar_spectra/_version.py"
+VERSIONFILE = "pulsar_spectra/_version.py"
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
@@ -24,15 +25,23 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-setup(name="pulsar_spectra",
+setup(
+    name="pulsar_spectra",
     version=verstr,
     description="A simple interface to record pulsar's flux density measurements for a large number of papers and perform fitting of spectral models. ",
     url="https://github.com/NickSwainston/pulsar_spectra.git",
-    packages=['pulsar_spectra'],
-    package_data={'pulsar_spectra':['catalogue_papers/*.yaml', 'catalogue_papers/*.db', 'configs/*.yaml', 'configs/*.csv']},
+    packages=["pulsar_spectra"],
+    package_data={
+        "pulsar_spectra": [
+            "catalogue_papers/*.yaml",
+            "catalogue_papers/*.db",
+            "configs/*.yaml",
+            "configs/*.csv",
+        ]
+    },
     scripts=["scripts/quick_fit.py", "scripts/build_plotting_config.py"],
-    python_requires='>=3.7',
+    python_requires=">=3.8",
     install_requires=reqs,
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest']
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
 )
