197bef98f0a855ca898538afde7dbaeaba3ad38f
NickSwainston <nickaswainston@gmail.com>
2025-10-02 08:03:32 +0800

    Updated the spectral fit test to be parametrized and use pulsars that are clearly the best fit before and after the uncertainty changes

--- tests/test_spectral_fit.py
+++ tests/test_spectral_fit.py
@@ -4,95 +4,50 @@ Tests the spectral_fit.py script
 """
 
 import numpy as np
+import pytest
 
 from pulsar_spectra.catalogue import collect_catalogue_fluxes
 from pulsar_spectra.spectral_fit import find_best_spectral_fit
 
-
-def test_find_best_spectral_fit():
+spectral_fit_tests = [
+    ("J0415+6954", "simple_power_law", ['Bilous_2016', 'Lorimer_1995b', 'Seiradakis_1995', 'Sanidas_2019', 'Dewey_1985', 'McEwen_2020', 'Malofeev_2000']),
+    ("J1327-6222", "broken_power_law", ['Jankowski_2018', 'Johnston_1992', 'Murphy_2017', 'Jankowski_2019', 'Bates_2011', 'Mantovanini_2025', 'Manchester_1978a', 'Hobbs_2004a', 'Johnston_2018', 'Keith_2024', 'van_Ommen_1997']),
+    ("J0125-2327", "high_frequency_cut_off_power_law", ['McEwen_2020', 'Lee_2025', 'Gitika_2023']),
+    ("J0024-7204C", "low_frequency_turn_over_power_law", ['Zhang_2019', 'Robinson_1995']),
+    ("J0304+1932", "double_turn_over_spectrum", ['Kumar_2025', 'Frail_2016', 'Han_1999', 'Seiradakis_1995', 'Bilous_2020', 'Slee_1986', 'Stovall_2015', 'Deneva_2024', 'Keith_2024', 'Kravtsov_2022', 'Kijak_1998', 'Bhat_2023', 'Malofeev_2000', 'Izvekova_1981', 'Hoensbroech_1997', 'Manchester_1978a', 'Johnston_2018', 'Weisberg_1999', 'Bilous_2016', 'Lorimer_1995b']),
+]
+@pytest.mark.parametrize("pulsar, exp_model_name, frozen_refs", spectral_fit_tests)
+def test_find_best_spectral_fit(pulsar, exp_model_name, frozen_refs):
     """Tests the find_best_spectral_fit funtion."""
     # limit the input publications to prevent future additions making the tests fail
-    cat_list = collect_catalogue_fluxes(
-        only_use=[
-            "Jankowski_2018",
-            "Keith_2011",
-            "Johnston_1993",
-            "Jankowski_2019",
-            "Bondonneau_2020",
-            "Dai_2015",
-            "Manchester_1996",
-            "Bell_2016",
-            "Fake data",
-            "Toscano_1998",
-            "Dewey_1985",
-            "Bilous_2016",
-            "Malofeev_2000",
-            "Hobbs_2004a",
-            "Murphy_2017",
-            "Lee_2022",
-            "Bilous_2020",
-            "Kramer_1998",
-            "Manchester_1978a",
-            "Spiewak_2022",
-            "Zhang_2019",
-            "Lorimer_1995b",
-            "Izvekova_1981",
-            "Zhao_2019",
-            "Johnston_2018",
-            "Bhat_2023",
-            "Bartel_1978",
-            "van_Ommen_1997",
-            "Xue_2017",
-            "Zakharenko_2013",
-            "Sanidas_2019",
-            "Robinson_1995",
-            "McEwen_2020",
-            "Frail_2016",
-        ]
-    )
+    cat_list = collect_catalogue_fluxes(only_use=frozen_refs)
+    # Use some ref markers to test they can be altered
     ref_markers = {
         "Jankowski_2018": ("k", "d", 7),  # black thin diamond
         "Jankowski_2019": ("#b6dbff", "*", 9),  # light blue star
         "Xue_2017": ("y", "P", 7.5),  # yellow thick plus)
     }
-    pulsars = [
-        ("J0415+6954", "simple_power_law"),
-        ("J0543+2329", "broken_power_law"),
-        ("J1703-1846", "high_frequency_cut_off_power_law"),
-        ("J0024-7204C", "low_frequency_turn_over_power_law"),
-        ("J1932+1059", "double_turn_over_spectrum"),
-    ]
-    refs_needed = []
-    for pulsar, exp_model_name in pulsars:
-        print(f"\nFitting {pulsar}")
-        freq_all, band_all, flux_all, flux_err_all, ref_all = cat_list[pulsar]
-        if pulsar == "J1932+1059":
-            # Add an extra point to encourage a high frequency cut off
-            freq_all.append(25000)
-            band_all.append(1000)
-            flux_all.append(0.01)
-            flux_err_all.append(0.001)
-            ref_all.append("Fake data")
-        for freq, band, flux, flux_err, ref in zip(freq_all, band_all, flux_all, flux_err_all, ref_all):
-            print(f"{float(freq):8.1f}{float(band):8.1f}{float(flux):12.4f}{float(flux_err):12.4f} {str(ref):20s}")
-            refs_needed.append(ref)
-        model_name, iminuit_result, fit_info, p_best, p_category = find_best_spectral_fit(
-            pulsar,
-            freq_all,
-            band_all,
-            flux_all,
-            flux_err_all,
-            ref_all,
-            plot_compare=True,
-            ref_markers=ref_markers,
-        )
-        for p, v, e in zip(iminuit_result.parameters, iminuit_result.values, iminuit_result.errors):
-            if p.startswith("v"):
-                print(f"{p} = {v / 1e6:8.1f} +/- {e / 1e6:8.1} MHz")
-            else:
-                print(f"{p} = {v:.5f} +/- {e:.5}")
-        np.testing.assert_string_equal(model_name, exp_model_name)
-    print(f"Refs needed:{list(set(refs_needed))}")
+
+    print(f"\nFitting {pulsar}")
+    freq_all, band_all, flux_all, flux_err_all, ref_all = cat_list[pulsar]
+    for freq, band, flux, flux_err, ref in zip(freq_all, band_all, flux_all, flux_err_all, ref_all):
+        print(f"{float(freq):8.1f}{float(band):8.1f}{float(flux):12.4f}{float(flux_err):12.4f} {str(ref):20s}")
+    model_name, iminuit_result, fit_info, p_best, p_category = find_best_spectral_fit(
+        pulsar,
+        freq_all,
+        band_all,
+        flux_all,
+        flux_err_all,
+        ref_all,
+        plot_compare=True,
+        ref_markers=ref_markers,
+    )
+    for p, v, e in zip(iminuit_result.parameters, iminuit_result.values, iminuit_result.errors):
+        if p.startswith("v"):
+            print(f"{p} = {v / 1e6:8.1f} +/- {e / 1e6:8.1} MHz")
+        else:
+            print(f"{p} = {v:.5f} +/- {e:.5}")
+    np.testing.assert_string_equal(model_name, exp_model_name)
 
 
 def test_plot_methods():
